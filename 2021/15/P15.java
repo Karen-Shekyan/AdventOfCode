ecb006ed11f13bfacbc39cd4e8b8d14b97a60bde
Karen-Shekyan <kshekyan30@stuy.edu>
2021-12-16 18:09:49 -0500

    done day 15

--- 2021/15/P15.java
+++ 2021/15/P15.java
@@ -3,8 +3,56 @@ import java.io.*;
 
 public class P15 {
 
-  public static int Dijkstra () {
+  public static int Dijkstra (int[][] grid, ArrayList<int[]> frontier, int[][] costs) {
+    while (frontier.size() != 0) {
+      int[] current = {10000000,-1,-1};
+      int index = 0;
+      for (int i = 0; i < frontier.size(); i++) {
+        if (frontier.get(i)[0] < current[0]) {
+          current = frontier.get(i);
+          index = i;
+        }
+      }
+      frontier.remove(index);
+      //System.out.println(Arrays.toString(current));
 
+      int r = current[1];
+      int c = current[2];
+      int newC = 0;
+      if (r + 1 < grid.length) {
+        newC = costs[r][c] + grid[r+1][c];
+        if (newC < costs[r+1][c]) {
+          costs[r+1][c] = newC;
+          int[] node = {newC,r+1,c};
+          frontier.add(node);
+        }
+      }
+      if (r - 1 >= 0) {
+        newC = costs[r][c] + grid[r-1][c];
+        if (newC < costs[r-1][c]) {
+          costs[r-1][c] = newC;
+          int[] node = {newC,r-1,c};
+          frontier.add(node);
+        }
+      }
+      if (c + 1 < grid[0].length) {
+        newC = costs[r][c] + grid[r][c+1];
+        if (newC < costs[r][c+1]) {
+          costs[r][c+1] = newC;
+          int[] node = {newC,r,c+1};
+          frontier.add(node);
+        }
+      }
+      if (c - 1 >= 0) {
+        newC = costs[r][c] + grid[r][c-1];
+        if (newC < costs[r][c-1]) {
+          costs[r][c-1] = newC;
+          int[] node = {newC,r,c-1};
+          frontier.add(node);
+        }
+      }
+    }
+    return costs[costs.length-1][costs[0].length-1];
   }
 
   public static void main(String[] args) throws IOException {
@@ -24,8 +72,20 @@ public class P15 {
       //System.out.println(Arrays.toString(grid[i]));
     }
 
-    PriorityQueue frontier = new PriorityQueue();
+    ArrayList<int[]> frontier = new ArrayList<int[]>();
+    int[] node1 = {grid[0][0],0,0};
+    frontier.add(node1);
+    // ArrayList<int[]> from = new ArrayList<int[]>;
+    // from.add()
+    int[][] costs = new int[grid.length][grid[0].length];
+    for (int i = 0; i < costs.length; i++) {
+      for (int j = 0; j < costs[i].length; j++) {
+        if (!(i == 0 && j == 0)) {
+          costs[i][j] = 10000000;
+        }
+      }
+    }
 
-    System.out.println(Dijkstra());
+    System.out.println(Dijkstra(grid, frontier, costs));
   }
 }
---
+++ 2021/15/P15_2.java
@@ -0,0 +1,99 @@
+import java.util.*;
+import java.io.*;
+
+public class P15_2 {
+
+  public static int Dijkstra (int[][] grid, ArrayList<int[]> frontier, int[][] costs) {
+    while (frontier.size() != 0) {
+      int[] current = {10000000,-1,-1};
+      int index = 0;
+      for (int i = 0; i < frontier.size(); i++) {
+        if (frontier.get(i)[0] < current[0]) {
+          current = frontier.get(i);
+          index = i;
+        }
+      }
+      frontier.remove(index);
+      //System.out.println(Arrays.toString(current));
+
+      int r = current[1];
+      int c = current[2];
+      int newC = 0;
+      if (r + 1 < grid.length) {
+        newC = costs[r][c] + grid[r+1][c];
+        if (newC < costs[r+1][c]) {
+          costs[r+1][c] = newC;
+          int[] node = {newC,r+1,c};
+          frontier.add(node);
+        }
+      }
+      if (r - 1 >= 0) {
+        newC = costs[r][c] + grid[r-1][c];
+        if (newC < costs[r-1][c]) {
+          costs[r-1][c] = newC;
+          int[] node = {newC,r-1,c};
+          frontier.add(node);
+        }
+      }
+      if (c + 1 < grid[0].length) {
+        newC = costs[r][c] + grid[r][c+1];
+        if (newC < costs[r][c+1]) {
+          costs[r][c+1] = newC;
+          int[] node = {newC,r,c+1};
+          frontier.add(node);
+        }
+      }
+      if (c - 1 >= 0) {
+        newC = costs[r][c] + grid[r][c-1];
+        if (newC < costs[r][c-1]) {
+          costs[r][c-1] = newC;
+          int[] node = {newC,r,c-1};
+          frontier.add(node);
+        }
+      }
+    }
+    return costs[costs.length-1][costs[0].length-1];
+  }
+
+  public static void main(String[] args) throws IOException {
+    File f = new File (args[0]);
+    Scanner sc = new Scanner (f);
+
+    ArrayList<String> data1 = new ArrayList<String> ();
+    while (sc.hasNextLine()) {
+      data1.add(sc.nextLine());
+    }
+
+    int[][] grid = new int[5*data1.size()][5*data1.get(0).length()];
+    for (int i = 0; i < grid.length; i++) {
+      int m = i / data1.size();
+      for (int j = 0; j < grid[0].length; j++) {
+        int n = j / data1.get(0).length();
+        int r = i%data1.size();
+        int c = j%data1.get(0).length();
+        //System.out.println(r + " " + c);
+        grid[i][j] = Integer.parseInt(data1.get(r).substring(c,c+1)) + n + m;
+        if (grid[i][j] > 9) {
+          grid[i][j] -= 9;
+        }
+      }
+      //System.out.println(Arrays.toString(grid[i]));
+    }
+
+    ArrayList<int[]> frontier = new ArrayList<int[]>();
+    int[] node1 = {grid[0][0],0,0};
+    frontier.add(node1);
+    // ArrayList<int[]> from = new ArrayList<int[]>;
+    // from.add()
+    int[][] costs = new int[grid.length][grid[0].length];
+    for (int i = 0; i < costs.length; i++) {
+      for (int j = 0; j < costs[i].length; j++) {
+        if (!(i == 0 && j == 0)) {
+          costs[i][j] = 10000000;
+        }
+      }
+    }
+
+    System.out.println(Dijkstra(grid, frontier, costs));
+  }
+}
